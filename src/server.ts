ec78f7b4df0368ab11fda5045c677e7939757667
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-06-12 21:17:50 -0300

    update links cors

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ app.use(cookieParser());
 const whiteList = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
-  "https://task-manager-gamma-plum.vercel.app/",
+  "https://task-manager-gamma-plum.vercel.app",
 ];
 
 app.use(
